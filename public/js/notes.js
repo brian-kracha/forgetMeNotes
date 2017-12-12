63d385d80d7823ced3148901c82c3815ab7e98df
SeanDBno <seanbonorden1@live.com>
2017-12-12 14:48:39 -0700

    added new routes, added dynamic DOM creation

--- public/js/notes.js
+++ public/js/notes.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
   $('.collapsible').collapsible();
   $('.modal').modal();
+
+  function addNote (title,content, priority, comments) {
+    return `<li> <div class="collapsible-header">${title}
+        <span class="badge"><i class='material-icons chatBubble'>chat_bubble_outline</i>${comments}
+        <i class='material-icons ${priority}'>fiber_manual_record</i></span></div>
+        <div class="collapsible-body"><span>${content}</span></div></li>`
+  }
+  function addCategory (title, color){
+    return `<div class="carousel-item ${color} white-text" href="#one!">
+            <h2>${Title}</h2>
+            </div>`
+  }
 });
