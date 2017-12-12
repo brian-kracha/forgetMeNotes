24ff8f04d59dfe9094fc97e023414a68a62bfa6c
brain-kracha <brian.kracha@gmail.com>
2017-12-12 13:17:57 -0700

    cloudinary api installed

---
+++ public/cloudinary/api.js
@@ -0,0 +1,27 @@
+const CLOUDINARY_URL = 	'https://api.cloudinary.com/v1_1/quartertwo/upload';
+const CLOUDINARY_UPLOAD_PRESET = 'wyaejzmi';
+
+
+var imgPreview = document.getElementById('img-preview');
+var fileUpload = document.getElementById('file-upload');
+
+fileUpload.addEventListener('change',function(event){
+  var file = event.target.files[0]
+  var formData = new FormData();
+  formData.append('file',file);
+  formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
+
+  axios({
+    url: CLOUDINARY_URL,
+    method: 'POST',
+      headers:{
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      data: formData
+  }).then(function(res){
+    console.log(res);
+    imgPreview.src = res.data.secure_url;
+  }).catch(function(err){
+    console.error(err);
+  })
+})
